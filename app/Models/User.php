2a1333154354c882233cb01308ca3a3e4a666b26
Javier Luna <lunaion2200@gmail.com>
2023-01-24 22:39:02 -0500

    Migraciones y modelos de Users y Profiles

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // Relación uno a uno
+    public function profile(){
+        return $this->hasOne(Profile::class);
+    }
 }
